6c5398cefd922444e028c1b1cb5e4046f08a91ed
salsaunleashed <127354335+salsaunleashed@users.noreply.github.com>
2023-11-21 00:16:21 +0000

    V0.7

---
+++ cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd "/cygdrive/c/Users/ajord/OneDrive/Ambiente de Trabalho/ISEC/POO/tpPOO/cmake-build-debug"
+/cygdrive/c/Users/ajord/AppData/Local/JetBrains/CLion2023.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
